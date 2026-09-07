2724837e77a3dd3c609bc37e8ac33cf4026527a6
code-with-ayyan <ayyanahmed2356@gmail.com>
2026-09-07 19:34:34 +0500

    Ensemble learning bagging classifier scratch implemented

---
+++ ensemble-learning/BaggingClassifier/BaggingClassifier.py
@@ -0,0 +1,55 @@
+import numpy as np 
+from copy import deepcopy
+
+class CustomBaggingClassifier:
+    
+    def __init__(self,
+                 base_estimator, 
+                 n_estimators=10,
+                 max_samples =1.0,
+                 bootstrap=True):
+        self.base_estimator = base_estimator
+        self.n_estimators = n_estimators
+        self.max_samples = max_samples
+        self.bootstrap = bootstrap
+        self.estimators_ = []
+        
+        
+    def bootstrap_sample(self, X, y):
+        n_samples = X.shape[0]
+        if self.bootstrap:
+            indices = np.random.choice(n_samples, size=int(self.max_samples * n_samples), replace=True)
+        else:
+            indices = np.random.choice(n_samples, size=int(self.max_samples * n_samples), replace=False)
+        return X[indices], y[indices]
+    
+    
+    def majority_vote(self, predictions):
+        result = []
+
+        for prediction in predictions.T:
+            result.append(np.bincount(prediction).argmax())
+
+        return np.array(result)
+    
+    def fit(self, X, y):
+        
+        self.estimators_ = []
+        
+        for _ in range(self.n_estimators):
+            X_sample, y_sample = self.bootstrap_sample(X, y)
+            estimator = deepcopy(self.base_estimator)
+            estimator.fit(X_sample, y_sample)
+            self.estimators_.append(estimator)
+            
+        return self
+    
+    def _predict(self, X):
+        predictions = np.array([estimator.predict(X) for estimator in self.estimators_])
+        return predictions
+    
+    def predict(self, X):
+        
+        predictions = self._predict(X)
+        majority_votes = self.majority_vote(predictions)
+        return majority_votes
